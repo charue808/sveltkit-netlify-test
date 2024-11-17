a5d3d17d960138881fb110bccee70647cb4a1655
charue808 <charlesgum@gmail.com>
2024-11-17 11:59:08 -1000

    add in form

--- src/routes/support-us/+page.svelte
+++ src/routes/support-us/+page.svelte
@@ -3,3 +3,19 @@
 	<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 </div>
 
+<form name="test" method="post" action="/success">
+	<input type="hidden" name="form-name" value="test" />
+	<input type="text" name="bot-field" />
+	<p>
+			<label>Your Name: <input type="text" name="name" /></label>
+	</p>
+	<p>
+			<label>Your Email: <input type="email" name="email" /></label>
+	</p>
+	<p>
+			<label>Message: <textarea name="message" /></label>
+	</p>
+	<p>
+			<button type="submit">Send</button>
+	</p>
+</form>
