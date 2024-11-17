1387fa7c0e9bf8af6bf0165169b7a3d27f5c8c65
charue808 <charlesgum@gmail.com>
2024-11-17 12:47:46 -1000

    add in server action

--- src/routes/shinnenkai/+page.server.ts
+++ src/routes/shinnenkai/+page.server.ts
@@ -1,8 +1,19 @@
 import type { Actions } from './$types';
 
 export const actions = {
-	rsvp: async ({ request }) => {
+	rsvp: async ({ url, request }) => {
 		const formData = await request.formData();
 		console.log('form data:', formData)
+
+		let origin = url.origin
+
+		fetch(`${origin}/shinnenkai`, {
+			method: "POST",
+			headers: { "Content-Type": "application/x-www-form-urlencoded" },
+			body: new URLSearchParams(formData).toString()
+		})
+			.then(() => alert("Thank you for your submission"))
+			.catch(error => alert(error));
+
 	}
 } satisfies Actions;
