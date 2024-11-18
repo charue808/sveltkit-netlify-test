9183333249024e883517cde5a0d468138e540ea5
charue808 <charlesgum@gmail.com>
2024-11-17 14:14:11 -1000

    change back to default and remove action

--- src/routes/shinnenkai/+page.server.ts
+++ src/routes/shinnenkai/+page.server.ts
@@ -1,8 +1,8 @@
-import { fail } from '@sveltejs/kit';
+
 import type { Actions } from './$types';
 
 export const actions = {
-	rsvp: async ({ url, request }) => {
+	default: async ({ url, request }) => {
 		const formData = await request.formData();
 		console.log('form data:', formData)
 
