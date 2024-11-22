f75976bf2bdd4331d826e61573f1983f49b2326b
charue808 <charlesgum@gmail.com>
2024-11-21 23:31:09 -1000

    remove import of lib md file

--- src/routes/become-a-member/+page.svelte
+++ src/routes/become-a-member/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Markdown from 'svelte-exmarkdown';
+	let { title, content } = $props();
 	let md = $state('# Membership!!!');
 	
 	</script>
@@ -9,5 +10,13 @@
 		<div class="content">
 			<Markdown {md} />
 		</div>
+		</div>
+	</section>
+	<section>
+		<div class="container">
+			<div class="content">
+				<h1>{title}</h1>
+				<p>{content}</p>
+			</div>
 		</div>
 	</section>
--- src/routes/become-a-member/+page.ts
+++ src/routes/become-a-member/+page.ts
@@ -1,10 +1,7 @@
 import { error } from '@sveltejs/kit';
-import md from '$lib/pages/become-a-member.md';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = ({  }) => {
-
-	console.log('md', md)
 
 	return {
 		title: 'Hello world!',
